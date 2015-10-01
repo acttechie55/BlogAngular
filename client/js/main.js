2f96f1cefd7684166cfcbd03a4adbb87f46bad27
acttechie55 <actaylor55@gmail.com>
2015-09-30 21:00:46 -0500

    Blog Angular Draft3

--- client/js/main.js
+++ client/js/main.js
@@ -1,5 +1,21 @@
 //Use to instantiate app, connect factory & controllers and configure app.
 
-var app = angular.module('', []);
+var myBlogApp = angular.module('myBlogApp', ['ngRoute', 'ngResource']);
+myBlogApp.controller('BlogController', ['$scope', '$http', function($scope, $http){
+	
+}]);
 
-app.config([]);
+myBlogApp.config(function($routeProvider){
+	$routeProvider
+		.when('/', {
+	//	controller: 'PostController',
+		templateUrl: '../views/blogposts.html'	
+	})
+		.when('latest', {
+	//	controller: 'SinglePostController',
+		templateUrl: '../views/newpost.html'
+	})
+		.otherwise ({
+		redirectTo: '/'
+	});
+});
